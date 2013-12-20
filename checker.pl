587a95bd0ddd880836ffb2e30b046d0ae8b5fc75
BBB <rkrasowski@yahoo.com>
2013-12-20 22:04:04 +0000

    checker.pl finished, check time stamp of the program if not updated (program not runnung or crashed), kills the program (if existed)and restart it

--- checker.pl
+++ checker.pl
@@ -2,35 +2,50 @@
 use strict;
 use warnings;
 
+############################################## checker.pl #######################
+#										#
+#	Check if program send time stamp to log, if no time stamp, will kill 	#	
+#	the program and restart it						#
+#	by Robert J. Krasowski							#
+#	12/20/2013								#
+#										#
+#################################################################################
 
-my $logFile = "./life.log";
-my $programChecked = "program.pl";
-my $logTime;
 
-
+my $timeFile = "./life.log";			# file to store time recorded by program ( should be in RAM)
+my $programChecked = "program.pl";		# program that will be checked
+my $waitTime = 10;				# time between checks in sec
+my $restartLog = "/var/log/restart.log";	# log where restart events will be recorded
+my $logTime;			
 my $PID;
 
 
 while(1)
 	{
 
-		open (my $LOG,"<" ,$logFile) || die "Failed to open $logFile: $!\n";
-		while(<$LOG>)
+		open (my $TIMELOG,"<" ,$timeFile) || die "Failed to open $timeFile: $!\n";
+		while(<$TIMELOG>)
 			{
 				$logTime .= $_;
 				chomp $logTime;		
 			} 
-		close ($LOG);
+		close ($TIMELOG);
 
 		my $sysTime = time();
-		my $timeDiff = $sysTime - $logTime;
+		my $timeDiff = $sysTime - $logTime;	# compare system time with time stamp from log
                 print "LogTime: $logTime\nSysTyme: $sysTime\ntimeDiff: $timeDiff\n\n";
 		
-		if ($timeDiff > 10)
+		if ($timeDiff > $waitTime)
 			{
 				# check PID and kill it
 				$PID = `pgrep $programChecked`;
 				print "$programChecked PID is $PID, doesn't work, will kill it\n";
+				# put entry into log
+				open (my $LOG, ">>" , "$restartLog");
+				my $date = `date`;
+				chomp $date;
+				print $LOG "$date: $programChecked restarted\n";
+				close ($LOG);
 
 				if ($PID)
 					{
@@ -47,5 +62,6 @@ while(1)
 					}
 			}
 		$logTime = "";
-		sleep(10);
+		$PID = "";
+		sleep($waitTime+1);
 	}
